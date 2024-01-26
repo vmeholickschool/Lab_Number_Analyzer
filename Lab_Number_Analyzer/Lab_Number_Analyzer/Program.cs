1f006b875e343116707e76430fdc2cedebb4b193
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-26 10:37:04 -0500

    Updated

--- Lab_Number_Analyzer/Lab_Number_Analyzer/Program.cs
+++ Lab_Number_Analyzer/Lab_Number_Analyzer/Program.cs
@@ -3,54 +3,49 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Write("What is your name? ");
-        string userName = Console.ReadLine().ToLower().Trim();
-        // Ask for number 
+        
 
-        Console.Write("Enter an integer between 1 and 100 inclusive; ");
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine().Trim();
+
+        Console.Write("Enter an integer between 1 and 100 inclusive:");
         int userNumber = int.Parse(Console.ReadLine());
 
 
-        for (int i = 0; i < userNumber; i++)
-            if (i > 0 && i < 101)
-            {
-                continue;
-            }
-
-
-        // Use if/else statements to analyze the entered number
-        if (userNumber <= 0 && userNumber > 100)
+        if (userNumber <= 0 || userNumber > 100)
         {
-            Console.WriteLine($"{userNumber} is not valid.");
+            Console.WriteLine($"{userName}, {userNumber} is not valid.");
         }
         else if (userNumber % 2 == 1 && userNumber < 60)
         {
-            Console.WriteLine($"{userNumber} is Odd and less than 60.");
+            Console.WriteLine($"{userName}, {userNumber} is odd and less than 60.");
         }
         else if (userNumber % 2 == 0 && userNumber >= 2 && userNumber <= 24)
         {
-            Console.WriteLine($"{userNumber} is even and less than 25.");
+            Console.WriteLine($"{userName}, {userNumber} is even and less than 25.");
         }
         else if (userNumber % 2 == 0 && userNumber >= 26 && userNumber <= 60)
         {
-            Console.WriteLine("Even and between 26 and 60 inclusive.");
+            Console.WriteLine($"{userName}, {userNumber} is even and between 26 and 60");
         }
         else if (userNumber % 2 == 0 && userNumber > 60)
         {
-            Console.WriteLine($"{userNumber} is Even and greater than 60.");
+            Console.WriteLine($"{userName}, {userNumber} is Even and greater than 60.");
         }
         else if (userNumber % 2 == 1 && userNumber > 60)
         {
-            Console.WriteLine($"{userNumber} is Odd and greater than 60.");
+            Console.WriteLine($"{userName}, {userNumber} is Odd and greater than 60.");
         }
 
-        else if (userNumber <= 0 && userNumber > 100)
+        else
         {
-            Console.WriteLine($"{userNumber} is not valid.");
+            Console.WriteLine($"{userName}, {userNumber} is not valid.");
         }
 
 
 
-        Console.WriteLine($"Thank you, for using the Number Analyzer!");
+        Console.WriteLine($"{userName}, Thank you, for using the Number Analyzer!");
+
+        Console.ReadKey();
     }
 }
