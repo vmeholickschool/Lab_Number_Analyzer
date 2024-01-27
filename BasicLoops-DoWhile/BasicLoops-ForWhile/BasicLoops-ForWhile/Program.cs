eecc43aa644cbb4d93d02e927308d77170dd5510
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-27 13:08:34 -0500

    Created program that was hard

---
+++ BasicLoops-DoWhile/BasicLoops-ForWhile/BasicLoops-ForWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿// Prompt the user for a number.
+//Use a for loop to output all the numbers from that number to 0. 
+//After that loop finishes, use another loop to output all the numbers from 0 to that number.
+
+do
+{
+    Console.Write("Enter a number: ");
+    int userInput = int.Parse(Console.ReadLine());
+
+    
+    for (int i = userInput; i >= 0; i--)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine();
+    
+    for (int i = 0; i <= userInput; i++)
+    {
+        
+        if (i < userInput)
+        {
+            Console.Write(i + " ");
+        }
+        else
+        {
+            Console.Write(i);
+        }
+    }
+
+    Console.Write("\nDo you want to continue? (yes/no): ");
+    string continueInput = Console.ReadLine().ToLower();
+
+    if (continueInput != "yes")
+    {
+        Console.WriteLine("Goodbye!");
+        break;
+    }
+
+    Console.WriteLine(); 
+
+} while (true); 
