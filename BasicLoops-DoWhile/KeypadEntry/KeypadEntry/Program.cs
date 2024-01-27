6dbaa5c1df1f6ef0483bcffbd7bc135af2c82d45
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-27 13:48:13 -0500

    Created an app to open door lock

---
+++ BasicLoops-DoWhile/KeypadEntry/KeypadEntry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool doorLocked = true;
+
+        while (doorLocked)
+        {
+            Console.Write("Enter your keycode: ");
+            string userKeyCode = Console.ReadLine();
+
+            if (userKeyCode == "13579")
+            {
+                doorLocked = false;
+                Console.WriteLine("Welcome Home!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Code, please enter your keycode: ");
+            }
+
+        }
+        Console.ReadLine();
+    }
+
+}
