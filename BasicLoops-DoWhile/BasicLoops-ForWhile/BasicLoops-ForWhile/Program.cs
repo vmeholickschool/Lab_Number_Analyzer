2df79996579091f7455f3352be2865c4969b5179
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-27 13:27:51 -0500

    few upates and formatting

--- BasicLoops-DoWhile/BasicLoops-ForWhile/BasicLoops-ForWhile/Program.cs
+++ BasicLoops-DoWhile/BasicLoops-ForWhile/BasicLoops-ForWhile/Program.cs
@@ -2,41 +2,46 @@
 //Use a for loop to output all the numbers from that number to 0. 
 //After that loop finishes, use another loop to output all the numbers from 0 to that number.
 
-do
+internal class Program
 {
-    Console.Write("Enter a number: ");
-    int userInput = int.Parse(Console.ReadLine());
-
-    
-    for (int i = userInput; i >= 0; i--)
+    private static void Main(string[] args)
     {
-        Console.Write(i + " ");
-    }
-
-    Console.WriteLine();
-    
-    for (int i = 0; i <= userInput; i++)
-    {
-        
-        if (i < userInput)
+        do
         {
-            Console.Write(i + " ");
-        }
-        else
-        {
-            Console.Write(i);
-        }
+            Console.Write("Enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+
+            for (int i = userInput; i >= 0; i--)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= userInput; i++)
+            {
+
+                if (i < userInput)
+                {
+                    Console.Write(i + " ");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+
+            Console.Write("\nDo you want to continue? (y/n): ");
+
+            if (Console.ReadLine().ToLower().Trim() != "y")
+            {
+                Console.WriteLine("Goodbye!");
+                break;
+            }
+
+            Console.WriteLine();
+            
+        } while (true);
     }
-
-    Console.Write("\nDo you want to continue? (yes/no): ");
-    string continueInput = Console.ReadLine().ToLower();
-
-    if (continueInput != "yes")
-    {
-        Console.WriteLine("Goodbye!");
-        break;
-    }
-
-    Console.WriteLine(); 
-
-} while (true); 
+}
