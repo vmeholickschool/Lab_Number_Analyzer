a42a7d15e07942f4b2089398445ad39590f9f841
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-27 15:01:50 -0500

    Changed to do while

---
+++ BasicLoops-DoWhile/DoWhileDoorLockKeyPad/DoWhileDoorLockKeyPad/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool doorLocked = true;
+        int attempts = 0;
+
+        do
+        {
+            Console.Write("Enter your keycode: ");
+            string userKeyCode = Console.ReadLine();
+
+            if (userKeyCode == "13579")
+            {
+                doorLocked = false;
+                Console.WriteLine("Welcome Home!");
+            }
+            else
+            {
+                attempts++;
+
+                if (attempts < 5)
+                {
+                    Console.WriteLine("Wrong Code, please enter the correct keycode.");
+                }
+                else
+                {
+                    Console.WriteLine("You have now made too many incorrect attempts, the door cannot be unlocked at this time.");
+                    break;
+                }
+            }
+        } while (doorLocked && attempts < 5);
+
+        Console.ReadLine();
+    }
+}
