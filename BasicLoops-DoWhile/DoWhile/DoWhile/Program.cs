693f75570df6e90ac8fd01c782ca770e9e362042
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-27 12:05:43 -0500

    Create do while program

---
+++ BasicLoops-DoWhile/DoWhile/DoWhile/Program.cs
@@ -0,0 +1,11 @@
+﻿// Use a do-while loop to output "Hello, World!" in a loop
+//Each time you output "Hello, World!" ask the user whether they would like to continue.
+
+bool userContinue;
+do
+{
+    Console.WriteLine("Hello, World!\n Would you like to continue (yes/no) ?");
+    userContinue = Console.ReadLine().ToLower().Trim() == "yes" ? true : false;
+    } while (userContinue == true);
+Console.WriteLine("Goodbye");
+Console.ReadKey();
