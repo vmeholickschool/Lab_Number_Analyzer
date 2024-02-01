50cfd983faf3a4f650a671b937b4b774bd34c1bb
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-31 21:25:09 -0500

    Starting Dice Roll Game

---
+++ DiceRollerLab/DiceRoller/DiceRoller/Program.cs
@@ -0,0 +1,27 @@
+﻿//Create an application that simulates dice rolling
+
+using System;
+using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random randomGenerator = new Random();
+        int diceRoll = randomGenerator.Next(1,30);
+
+        Console.WriteLine("Welcome to Grand Circus Casino! ");
+        Console.WriteLine("Please enter the number of sides for a pair of dice: ");
+        string line = Console.ReadLine();
+        
+        try
+        {
+            int num = int.Parse(line);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Your response is invalid, please enter a numeric value: ");
+        }
+        
+    }
+}
